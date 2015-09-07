fc0d3e8c93fa1f591e71fab343e039e1e776fd21
alagunambi <alagunambiwelkin@gmail.com>
2015-09-07 19:51:38 +0530

    Redirection bug fixed in visitors controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -7,15 +7,15 @@ class VisitorsController < ApplicationController
     schedule = Schedule.find_or_create_by(:tag_id => tag.id, :date => params[:schedule][:date])
 
     #@ads = from_haraj(schedule)
-    @ads = Ad.all
+    #@ads = Ad.all
 
-    job = Sidekiq::Cron::Job.create(name: schedule.id.to_s + "-" + Time.now.to_i.to_s, cron: generate_cron(schedule), klass: 'Puller', :args => {:schedule => schedule.id} )
+    @job = Sidekiq::Cron::Job.create(name: schedule.id.to_s + "-" + Time.now.to_i.to_s, cron: generate_cron(schedule), klass: 'Puller', :args => {:schedule => schedule.id} )
 
     respond_to do |format|
-      if @ads.count > 1
-        format.html { redirect_to schedules_path, notice: 'Scheduled Ads gathering successfully.' }
+      if job.count > 0
+        format.html { redirect_to schedules_path, notice: 'Scheduled Ads gathering will start at 9AM.' }
       else
-        format.html { redirect_to :back, notice: 'Failed to gather information.' }
+        format.html { redirect_to :back, notice: 'Failed to schedule, please try again.' }
       end
     end
 
